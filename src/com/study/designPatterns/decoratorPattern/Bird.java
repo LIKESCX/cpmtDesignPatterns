35fc3cec6ae090c4ddd62b10c463f568c173d70a
admin <shangchuanxiang@126.com>
2020-01-11 10:16:57 +0800

    sumbit by me

---
+++ src/com/study/designPatterns/decoratorPattern/Bird.java
@@ -0,0 +1,12 @@
+package com.study.designPatterns.decoratorPattern;
+/**
+ * @author shangchuanxiang
+ * @date 2020年1月11日10:12:41
+ * @description 装饰模式
+ */
+//1. 抽象组件
+public abstract class Bird {
+	
+	public abstract int fly();
+	
+}
